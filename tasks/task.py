40afc0eee08dfe5c1066dd3846d6ea83f5eb332f
Gruzin <gruzd.roman@inbox.ru>
2023-03-09 22:57:29 +0300

    Commit with a solution

--- tasks/task.py
+++ tasks/task.py
@@ -2,8 +2,13 @@ def check_str(s: str):
     """
     Add your code here
     """
-    new_str = s[::-1]
-    if s == new_str:
+    new_s = ''
+    for i in s.lower():
+        if i.isalnum():
+            new_s += i
+
+    new_str = new_s[::-1]
+    if new_s == new_str:
         return True
     else:
         return False
